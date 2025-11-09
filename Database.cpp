4fbf3f9318b49cfbc4cff5741fe591016bf5cf54
Pranav Kandpal <pranav.kandpal05@gmail.com>
2025-11-09 16:28:51 +0530

    final database

--- Database.cpp
+++ Database.cpp
@@ -1,31 +1,44 @@
 #include<iostream>
 #include<unordered_map>
+#include<optional>
+#include <cstdint> // for uint64 -> fixed on all platforms unlike long
+
+using Id = std::uint64_t;
 
 class Database{
     private:
-        std::unordered_map<long, std::string> db;
+        std::unordered_map<Id, std::string> db;
 
     public:
-        Database();
+        Database() = default;
 
-        void insert(const long id, const std::string document){
-            db.emplace(id, document); // better than insert({})
+        explicit Database(std::size_t capacity_hint) {
+            db.reserve(capacity_hint); // allocates memory ahead of time, restricts copying db1 = db2
         }
 
-        const std::string* get_id(long id) {   // use option<string> after update
+
+        void insert(const Id id, const std::string& document){
+            db.emplace(id, document); // better than insert({}) 
+        }
+
+        std::optional<std::string> getById(Id id) {
             auto it = db.find(id);
-            if (it != db.end()) 
-                return &it->second; // better than db.contains -> db.at(id) [O(logn) vs 2 x O(logn)]
-            return nullptr;
+            if (it != db.end()) return it->second; // better than db.contains -> db.at(id) [O(logn) vs 2 x O(logn)]
+            return std::nullopt;
         }
 
-        void update(const long id, std::string document){
+        void update(const Id id, std::string document){
             auto it = db.find(id);
             if (it != db.end())
                 it->second = document;
         }
 
-        bool remove(const long id){
+        bool removeById(const Id id){
             return db.erase(id) > 0; // .erase() takes care of not found case
         }
 };
+
+
+int main(){
+    return 0;
+}
